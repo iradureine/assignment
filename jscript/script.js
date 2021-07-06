4af5c83fe96a3214aa4569df7e6e5840b00f4b95
reineiradu <miradukunda601@gmail.com>
2021-07-06 04:38:51 -0700

    blogpost

---
+++ jscript/script.js
@@ -0,0 +1,10 @@
+
+var modal=document.getElementById('showmodal');
+var btn=document.getElementById('signinshow');
+var span=document.getElementById('close')[0];
+btn.onclick=function(){modal.style.display='block';
+    
+}
+span.onclick=function(){
+    modal.style.display='none';
+}
